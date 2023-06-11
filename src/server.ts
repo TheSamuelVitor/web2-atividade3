6a6329b6521800d43c2c953b27f606bbd646f446
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-06-11 18:53:22 -0300

    :wrench: chore: adicionando configuracao do typescript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "Página principal"
+  })
+})
+
+app.listen('3000', () => {
+  console.log("Servidor rodando na porta 3000")
+})

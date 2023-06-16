775b2b953d1c72053b55041a3bb7146164dc88f3
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2023-06-16 17:30:15 -0300

    :sparkles: feat: adicionando requisicao com socket

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import express from 'express'
+import { Server } from 'socket.io'
 
 const app = express()
+const port = 3000
+const io = new Server(port)
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    msg: 'Página principal',
+  res.status(200).sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('novo usuario conectado')
+  socket.on('mensageiro', (msg) => {
+    io.emit(msg)
   })
 })
 
-app.listen('3000', () => {
-  console.log('Servidor rodando na porta 3000')
+app.listen(port, () => {
+  console.log('Servidor rodando na porta '+port)
 })
